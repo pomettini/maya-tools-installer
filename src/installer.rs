83ea7b61132d7ce761f37dffd7aee00110297b8d
Giorgio Pomettini <giorgio.freespire@gmail.com>
2019-01-30 16:16:05 +0100

    Rustfmt

--- src/installer.rs
+++ src/installer.rs
@@ -1,131 +1,100 @@
-extern crate reqwest;
-extern crate os_type;
 extern crate dirs;
+extern crate os_type;
+extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 
-use std::io::{Write};
-use std::path::{PathBuf};
+use serde_json::Error;
 use std::fs::File;
 use std::io;
-use serde_json::{Error};
+use std::io::Write;
+use std::path::PathBuf;
 
 const AIV_SHELF_URL: &str = "https://www.giorgiopomettini.eu/aiv_shelf.json";
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Shelf 
-{
+pub struct Shelf {
     pub response: String,
     pub shelf_url: String,
     pub shelf_name: String,
     pub icons_url: String,
     pub icons_name: Vec<String>,
     pub icons_extension: String,
-    pub icons_variants: Vec<String>
+    pub icons_variants: Vec<String>,
 }
 
 #[derive(Debug, Default)]
-pub struct Icon
-{
+pub struct Icon {
     pub name: String,
-    pub data: Vec<u8>
+    pub data: Vec<u8>,
 }
 
-pub fn get_json_data() -> String
-{
-    match reqwest::get(AIV_SHELF_URL)
-    {
-        Ok(mut request) => 
-        {
-            match request.text()
-            {
-                Ok(text) => 
-                {
-                    info!("Shelf data downloaded");
-                    text
-                },
-                Err(error) => 
-                {
-                    error!("Shelf data downloaded but got error: {}", error);
-                    panic!();
-                }
+pub fn get_json_data() -> String {
+    match reqwest::get(AIV_SHELF_URL) {
+        Ok(mut request) => match request.text() {
+            Ok(text) => {
+                info!("Shelf data downloaded");
+                text
+            }
+            Err(error) => {
+                error!("Shelf data downloaded but got error: {}", error);
+                panic!();
             }
         },
-        Err(error) => 
-        {
+        Err(error) => {
             error!("Error downloading shelf data: {}", error);
             panic!();
         }
     }
 }
 
-pub fn get_shelf_data(data: &str) -> Result<Shelf, Error> 
-{
+pub fn get_shelf_data(data: &str) -> Result<Shelf, Error> {
     let shelf: Shelf = serde_json::from_str(data)?;
     Ok(shelf)
 }
 
-pub fn check_json(json_data: Result<Shelf, Error>) -> Shelf
-{
-    match json_data
-    {
-        Ok(shelf_data) => 
-        {
-            if shelf_data.response == "OK"
-            {
+pub fn check_json(json_data: Result<Shelf, Error>) -> Shelf {
+    match json_data {
+        Ok(shelf_data) => {
+            if shelf_data.response == "OK" {
                 info!("Shelf data OK");
                 shelf_data
-            }
-            else 
-            {
+            } else {
                 error!("Shelf data error");
                 panic!();
             }
-        },
-        Err(error) =>
-        {
+        }
+        Err(error) => {
             error!("Json cannot be parsed: {}", error);
             panic!();
         }
     }
 }
 
-pub fn download_shelf_file(shelf: &Shelf) -> String
-{
-    match reqwest::get(&format!("{}{}", &shelf.shelf_url, &shelf.shelf_name))
-    {
-        Ok(mut request) => 
-        {
-            match request.text()
-            {
-                Ok(text) => 
-                {
-                    info!("Shelf downloaded");
-                    text
-                },
-                Err(error) => 
-                {
-                    error!("Shelf downloaded but got error: {}", error);
-                    panic!();
-                }
+pub fn download_shelf_file(shelf: &Shelf) -> String {
+    match reqwest::get(&format!("{}{}", &shelf.shelf_url, &shelf.shelf_name)) {
+        Ok(mut request) => match request.text() {
+            Ok(text) => {
+                info!("Shelf downloaded");
+                text
+            }
+            Err(error) => {
+                error!("Shelf downloaded but got error: {}", error);
+                panic!();
             }
         },
-        Err(error) => 
-        {
+        Err(error) => {
             error!("Error downloading shelf: {}", error);
             panic!();
         }
     }
 }
 
-pub fn construct_icons_url(shelf: &Shelf) -> Vec<Icon>
-{
+pub fn construct_icons_url(shelf: &Shelf) -> Vec<Icon> {
     let mut icons: Vec<Icon> = Vec::new();
 
-    for icon in &shelf.icons_name
-    {
-        for variant in &shelf.icons_variants
-        {
+    for icon in &shelf.icons_name {
+        for variant in &shelf.icons_variants {
             let mut i: Icon = Default::default();
             i.name = format!("{}{}.{}", &icon, &variant, &shelf.icons_extension);
             icons.push(i);
@@ -135,64 +104,48 @@ pub fn construct_icons_url(shelf: &Shelf) -> Vec<Icon>
     icons
 }
 
-pub fn download_icons(shelf: &Shelf, icons: &mut Vec<Icon>)
-{
-    for icon in icons
-    {
+pub fn download_icons(shelf: &Shelf, icons: &mut Vec<Icon>) {
+    for icon in icons {
         info!("Downloading icon {}", &icon.name);
-        
-        match reqwest::get(&format!("{}{}", &shelf.icons_url, &icon.name))
-        {
-            Ok(mut request) =>
-            {
+
+        match reqwest::get(&format!("{}{}", &shelf.icons_url, &icon.name)) {
+            Ok(mut request) => {
                 let mut buffer: Vec<u8> = vec![];
-                match request.copy_to(&mut buffer)
-                {
-                    Ok(_size) =>
-                    {
+                match request.copy_to(&mut buffer) {
+                    Ok(_size) => {
                         icon.data = buffer;
-                    },
-                    Err(_error) => ()
+                    }
+                    Err(_error) => (),
                 }
-            },
-            Err(error) =>
-            {
+            }
+            Err(error) => {
                 warn!("Error downloading icon: {}", error);
             }
         }
     }
 }
 
-pub fn set_maya_directory() -> PathBuf
-{
-    match get_maya_directory()
-    {
-        Some(path) => 
-        {
+pub fn set_maya_directory() -> PathBuf {
+    match get_maya_directory() {
+        Some(path) => {
             info!("Found Maya directory");
             path
-        },
-        None => 
-        {
+        }
+        None => {
             error!("Maya directory not found:");
             panic!();
         }
     }
 }
 
-pub fn get_maya_installed_versions(maya_path: &PathBuf) -> Vec<usize>
-{
+pub fn get_maya_installed_versions(maya_path: &PathBuf) -> Vec<usize> {
     let mut maya_versions = Vec::new();
 
-    for entry in maya_path.read_dir().unwrap()
-    {
-        if let Ok(entry) = entry 
-        {
+    for entry in maya_path.read_dir().unwrap() {
+        if let Ok(entry) = entry {
             // Inefficent, needs refactor
-            for version in 2011..2020 
-            {
-                if entry.path().ends_with(version.to_string())
-                {
+            for version in 2011..2020 {
+                if entry.path().ends_with(version.to_string()) {
                     maya_versions.push(version);
                 }
             }
@@ -202,8 +155,7 @@ pub fn get_maya_installed_versions(maya_path: &PathBuf) -> Vec<usize>
     maya_versions
 }
 
-pub fn get_maya_shelf_directory(maya_path: &PathBuf, maya_version: &usize) -> Option<PathBuf>
-{
+pub fn get_maya_shelf_directory(maya_path: &PathBuf, maya_version: &usize) -> Option<PathBuf> {
     let mut shelf_directory = PathBuf::new();
 
     shelf_directory.push(&maya_path);
@@ -211,18 +163,14 @@ pub fn get_maya_shelf_directory(maya_path: &PathBuf, maya_version: &usize) -> Op
     shelf_directory.push("prefs");
     shelf_directory.push("shelves");
 
-    if shelf_directory.exists()
-    {
+    if shelf_directory.exists() {
         Some(shelf_directory)
-    }
-    else 
-    {
+    } else {
         None
     }
 }
 
-pub fn get_maya_icons_directory(maya_path: &PathBuf, maya_version: &usize) -> Option<PathBuf>
-{
+pub fn get_maya_icons_directory(maya_path: &PathBuf, maya_version: &usize) -> Option<PathBuf> {
     let mut icons_directory = PathBuf::new();
 
     icons_directory.push(&maya_path);
@@ -230,38 +178,30 @@ pub fn get_maya_icons_directory(maya_path: &PathBuf, maya_version: &usize) -> Op
     icons_directory.push("prefs");
     icons_directory.push("icons");
 
-    if icons_directory.exists()
-    {
+    if icons_directory.exists() {
         Some(icons_directory)
-    }
-    else 
-    {
+    } else {
         None
     }
 }
 
-pub fn get_maya_directory() -> Option<PathBuf>
-{
+pub fn get_maya_directory() -> Option<PathBuf> {
     let mut maya_directory = PathBuf::new();
 
-    match dirs::home_dir()
-    {
+    match dirs::home_dir() {
         Some(path) => maya_directory.push(path),
         None => panic!("Cannot get your HOME dir"),
     }
 
-    match os_type::current_platform().os_type 
-    {
-        os_type::OSType::OSX => 
-        {
+    match os_type::current_platform().os_type {
+        os_type::OSType::OSX => {
             maya_directory.push("Library");
             maya_directory.push("Preferences");
             maya_directory.push("Autodesk");
             maya_directory.push("maya");
-        },
+        }
         // This will probably be Windows, or maybe not
-        _ => 
-        {
+        _ => {
             maya_directory.push("Documents");
             maya_directory.push("maya");
         }
@@ -269,25 +209,20 @@ pub fn get_maya_directory() -> Option<PathBuf>
 
     info!("Maya directory: {:?}", &maya_directory);
 
-    if maya_directory.exists()
-    {
+    if maya_directory.exists() {
         Some(maya_directory)
-    }
-    else 
-    {
+    } else {
         None
     }
 }
 
-pub fn write_file(content: &str, path: &PathBuf) -> io::Result<()>
-{
+pub fn write_file(content: &str, path: &PathBuf) -> io::Result<()> {
     let mut file = File::create(path)?;
     file.write_all(&content.as_bytes())?;
     Ok(())
 }
 
-pub fn write_file_binary(content: &Vec<u8>, path: &PathBuf) -> io::Result<()>
-{
+pub fn write_file_binary(content: &Vec<u8>, path: &PathBuf) -> io::Result<()> {
     let mut file = File::create(path)?;
     file.write_all(&content)?;
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-extern crate reqwest;
-extern crate os_type;
 extern crate dirs;
+extern crate os_type;
+extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 #[macro_use]
@@ -9,14 +9,13 @@ extern crate serde_derive;
 extern crate log;
 extern crate fern;
 
-use std::path::{PathBuf};
+use std::path::PathBuf;
 
 pub mod installer;
 
 use installer::*;
 
-fn main() 
-{
+fn main() {
     fern::Dispatch::new()
         .level(log::LevelFilter::Info)
         .chain(std::io::stdout())
@@ -50,26 +49,25 @@ fn main()
 
     // Check which versions of Maya are installed
     let maya_installed_versions = get_maya_installed_versions(&maya_directory);
-    
+
     // For each Maya version:
-    for maya_version in maya_installed_versions
-    {
+    for maya_version in maya_installed_versions {
         info!("Now working on Maya version {}", maya_version);
 
         let mut maya_shelf_directory = PathBuf::new();
 
         // Get Maya shelf directory
         // Check if Maya shelf directory exists
-        match get_maya_shelf_directory(&maya_directory, &maya_version)
-        {
-            Some(path) => 
-            {
+        match get_maya_shelf_directory(&maya_directory, &maya_version) {
+            Some(path) => {
                 info!("Found shelf directory for Maya {}, moving on", maya_version);
                 maya_shelf_directory = path;
-            },
-            None => 
-            {
-                warn!("There is no shelf directory for Maya {}, moving to the next version", maya_version);
+            }
+            None => {
+                warn!(
+                    "There is no shelf directory for Maya {}, moving to the next version",
+                    maya_version
+                );
                 continue;
             }
         }
@@ -79,21 +77,20 @@ fn main()
         maya_file_shelf_path.push(&shelf.shelf_name);
 
         // Check if shelf file exist
-        if maya_file_shelf_path.exists()
-        {
+        if maya_file_shelf_path.exists() {
             warn!("Shelf already exists, will be overwritten");
         }
 
         // Write shelf file
-        match write_file(&shelf_content, &maya_file_shelf_path)
-        {
-            Ok(()) => 
-            {
+        match write_file(&shelf_content, &maya_file_shelf_path) {
+            Ok(()) => {
                 info!("Shelf writing complete");
-            },
-            Err(error) => 
-            {
-                warn!("Could not write shelf on the directory {:?}: {}", &maya_file_shelf_path, error);
+            }
+            Err(error) => {
+                warn!(
+                    "Could not write shelf on the directory {:?}: {}",
+                    &maya_file_shelf_path, error
+                );
             }
         }
 
@@ -101,43 +98,44 @@ fn main()
 
         // Get Maya icons directory
         // Check if Maya icons directory exists
-        match get_maya_icons_directory(&maya_directory, &maya_version)
-        {
-            Some(path) => 
-            {
+        match get_maya_icons_directory(&maya_directory, &maya_version) {
+            Some(path) => {
                 info!("Found icons directory for Maya {}, moving on", maya_version);
                 maya_icons_directory = path;
-            },
-            None => 
-            {
-                warn!("There is no icons directory for Maya {}, moving to the next version", maya_version);
+            }
+            None => {
+                warn!(
+                    "There is no icons directory for Maya {}, moving to the next version",
+                    maya_version
+                );
                 continue;
             }
         }
 
         // For each icon
-        for icon in &icons
-        {
+        for icon in &icons {
             // Get complete icon path with filename and extension
             let mut icon_path = PathBuf::from(&maya_icons_directory);
             icon_path.push(&icon.name);
-            
+
             // Check if icon file exists
-            if icon_path.exists()
-            {
-                warn!("File at {:?} already exists, will be overwritten", &icon_path);
+            if icon_path.exists() {
+                warn!(
+                    "File at {:?} already exists, will be overwritten",
+                    &icon_path
+                );
             }
 
             // Write icon file
-            match write_file_binary(&icon.data, &icon_path)
-            {
-                Ok(()) => 
-                {
+            match write_file_binary(&icon.data, &icon_path) {
+                Ok(()) => {
                     info!("Writing icon {} complete", &icon.name);
-                },
-                Err(error) => 
-                {
-                    warn!("Could not write icon on the directory {:?}: {}", &icon_path, error);
+                }
+                Err(error) => {
+                    warn!(
+                        "Could not write icon on the directory {:?}: {}",
+                        &icon_path, error
+                    );
                 }
             }
         }
